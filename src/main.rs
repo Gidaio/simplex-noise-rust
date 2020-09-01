30985f6fbc4a8b1285f5d78ffb74286f16d059a5
Gidaio <killshade@gmail.com>
2020-09-01 00:29:56 -0600

    random, too

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-#[macro_use]
 extern crate bmp;
 use bmp::{Image, Pixel};
+use rand::prelude::*;
 
 
 fn main() {
     let mut image = Image::new(256, 256);
 
     for (x, y) in image.coordinates() {
-        image.set_pixel(x, y, px!(x, y, 0))
+        image.set_pixel(x, y, Pixel::new(random(), random(), random()))
     }
 
     image.save("image.bmp").unwrap();
